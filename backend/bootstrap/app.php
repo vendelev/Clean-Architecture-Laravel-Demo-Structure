1aae47330f5513c1cab9e91e099a7d9af81c7aec
Венделев Артём <artiom@vendelev.ru>
2025-04-26 09:49:38 +0300

    Настройка healthcheck

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -10,7 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
-        health: '/up',
+        health: '/healthcheck',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
